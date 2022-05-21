2c18dac87c3cfdfa4e6c31791476f222058c07a0
pdivyansh20 <84575211+pdivyansh20@users.noreply.github.com>
2022-05-21 12:22:57 +0530

    commiteed

--- server/app.js
+++ server/app.js
@@ -1,12 +1,39 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-const app =  express() 
+dotenv.config({ path: "./config.env" });
+require(`./db/conn`);
 
-app.get("/", function(req,res){
-    res.send("hey")
-})
+const PORT = process.env.PORT;
 
-app.listen(4000, function(){
-    console.log("hey")
-})
+// function middleware( req,res,next) {
+//     console.log("hello friend")
+// }
+app.use(express.json());
+app.use(require("./router/auth"));
+
+// app.get("/", function(req,res){
+//     res.send("home")
+// })
+
+// app.get("/about", middleware , function(req,res){
+//     res.send("about")
+// })
+
+// app.get("/signin", function(req,res){
+//     res.send("signin")
+// })
+
+// app.get("/signup", function(req,res){
+//     res.send("signup")
+// })
+
+// app.get("/contact", function(req,res){
+//     res.send("contact")
+// })
+
+app.listen(PORT, function () {
+  console.log(`running at port ${PORT}`);
+});
---
+++ server/router/auth.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const bcrypt = require("bcryptjs") 
+const jwt = require("jsonwebtoken")
+const router = express.Router();
+require(`../db/conn`)
+const User = require("../model/userSchema")
+router.get("/", function(req,res){
+    res.send("home coming")
+})
+
+router.post('/register' , async function(req,res){
+    const{ name, email,phone,work,password,cpassword} = req.body
+
+              if (!name || !email ||!phone ||!work ||!password|| !cpassword) {
+      return res.json({eroor: "filled the required field" })
+    }
+    const userExist =  await User.findOne({email:email}) 
+        if(userExist){
+            return res.json({eroor: "email already exist" })
+        }
+    const user = new User({ name, email,phone,work,password,cpassword}) ;
+     
+    await user.save() ;
+    res.json({message:"registered succces"})
+    
+})
+
+router.post('/sigin', async function(req,res){
+     const {email , password} = req.body;
+        
+     if (!email || !password){
+         return res.json({error:"please fill the field"})
+     }
+     const userLogin = await User.findOne({email:email}) 
+     if(userLogin){
+        const isMatch = await bcrypt.compare(password,userLogin.password)
+        const token =  await userLogin.genrateAuthToken();
+        
+        res.cookie("jwtoken",token,{
+            expires: new Date(Date.now()+25892000000),
+            httpOnly:true
+        })
+        if (!isMatch) {
+       res.json({message : "user error"})
+        }
+       else {
+         res.json({message : "User  sigin succes"})
+        }
+     }
+     else {
+        res.json({message : "User error"})
+     }
+    
+})
+
+module.exports = router 
